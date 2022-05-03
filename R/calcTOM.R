93197951191429aa0611a9a932d83a007954607d
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2022-05-03 17:22:52 +0800

    init

---
+++ R/calcTOM.R
@@ -0,0 +1,37 @@
+
+#' Calculate an adjacency matrix
+#' @param cl An object of class coexList.
+#' @param ... Parameters for WGCNA::TOMsimilarity()
+#' @return A coexList object.
+#'
+#' @description A wrapper function for running the WGCNA::TOMsimilarity function
+#' on a coexList object
+#' @export
+#' @examples
+#' ngenes <- 1000
+#' nsamples <- 16
+#' edat <- matrix(rpois(ngenes*nsamples, lambda=5), nrow=ngenes)
+#' rownames(edat) <- paste0("gene_", 1:ngenes)
+#' cl <- coexList(counts = edat)
+#' cl <- normCounts(cl)
+#' cl <- calcAdjacency(cl, softPower=6)
+#' cl <- calcTOM(cl)
+#' # To view TOM
+#' cl@dissTOM[1:6, 1:6]
+
+calcTOM <- function(cl, ...){
+
+    cat("=== Running WGCNA::TOMsimilarity ===\n")
+    cat("...This might take a while...\n")
+    calc_start <- Sys.time()
+
+    cl@dissTOM <- 1 - WGCNA::TOMsimilarity(cl@adjacencyMat, ...)
+
+    calc_end <- Sys.time() - calc_start
+    cat(paste0("Elapsed time: ",
+               round(as.numeric(calc_end), digits = 2),
+               " ", units(calc_end), "\n"))
+    return(cl)
+}
+
+
