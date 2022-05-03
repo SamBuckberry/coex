740b754b09e670fbe31e0d9b5d2fe8528194e0ca
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2022-05-03 09:02:13 +0800

    Comment out use_vignette

--- setup.R
+++ setup.R
@@ -13,7 +13,8 @@ usethis::use_package("methods")
 usethis::use_package("ggplot2")
 usethis::use_package("SummarizedExperiment")
 usethis::use_testthat()
-usethis::use_vignette("my-vignette")
+#usethis::use_vignette("my-vignette")
+usethis::use_data_raw()
 # Files to ignore
 usethis::use_build_ignore("setup.R")
 
@@ -24,12 +25,3 @@ devtools::check()
 devtools::install()
 
 #######
-
-ngenes <- 1000
-nsamples <- 16
-edat <- matrix(rnorm(ngenes*nsamples,mean=5,sd=2),ngenes,nsamples)
-rownames(edat) <- 1:ngenes
-cl <- coexList(counts = edat)
-cl <- calcSoftPower(cl)
-
-
