80dffb32948dbffc53ca4af3975844b7205acf32
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2022-05-09 16:49:58 +0800

    add unit test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(coex)
+
+test_check("coex")
---
+++ tests/testthat/test-getModuleMembership.R
@@ -0,0 +1,17 @@
+
+ngenes <- 1000
+nsamples <- 16
+edat <- matrix(rpois(ngenes*nsamples, lambda=5), nrow=ngenes)
+rownames(edat) <- paste0("gene_", 1:ngenes)
+cl <- coexList(counts = edat)
+cl <- normCounts(cl)
+cl <- calcAdjacency(cl, softPower=6)
+cl <- calcTOM(cl)
+cl <- calcTree(cl)
+cl <- calcModuleEigengenes(cl)
+cl <- calcModuleMembership(cl)
+
+testthat::test_that("calcModuleMembership returns data.frame", {
+  testthat::expect_true(is.data.frame(getModuleMembership(cl)))
+})
+
