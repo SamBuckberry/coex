67f574b6e01a4374edc8d06d60b2523a4aebe00a
Sam Buckberry <SamBuckberry@users.noreply.github.com>
2022-05-09 16:18:34 +0800

    init

---
+++ R/getModuleMembership.R
@@ -0,0 +1,31 @@
+#' Get co-expression module membership data from coexList object
+#'
+#' @param cl An object of class coexList.
+#' @param ... Parameters for WGCNA::signedKME
+#' @return A coexList object. This function adds data to the 'moduleMembership'
+#' slot of a coexList object.
+#'
+#' @seealso \code{\link[WGCNA]{signedKME}}
+#' @export
+#' @examples
+#' ngenes <- 1000
+#' nsamples <- 16
+#' edat <- matrix(rpois(ngenes*nsamples, lambda=5), nrow=ngenes)
+#' rownames(edat) <- paste0("gene_", 1:ngenes)
+#' cl <- coexList(counts = edat)
+#' cl <- normCounts(cl)
+#' cl <- calcAdjacency(cl, softPower=6)
+#' cl <- calcTOM(cl)
+#' cl <- calcTree(cl)
+#' cl <- calcModuleEigengenes(cl)
+#' cl <- calcModuleMembership(cl)
+#' mm <- getModuleMembership(cl)
+#' mm[1:3, 1:3]
+#'
+getModuleMembership <- function(cl){
+
+    stopifnot("cl must be a coexList object" = class(cl)[1] == "coexList")
+
+    return(cl@moduleMembership)
+
+    }
